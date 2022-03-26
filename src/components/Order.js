bf0235e3f26731d3fcd742802b1faaf97a8ad8a2
smiana123@gmail.com <smiana123@gmail.com>
2022-03-26 14:01:27 +0300

    дадата

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 
+import { AddressSuggestions } from 'react-dadata';
+import 'react-dadata/dist/react-dadata.css';
+
 class Order extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state= {
+            address: ''
+        }
+    }
 
     render () {
         return (
             <Form>
-                <fieldset disabled>
+                <fieldset>
                     <Form.Group className="mb-3">
-                    <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
-                    <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+                    <Form.Label htmlFor="disabledTextInput"> input</Form.Label>
+                    <Form.Control id="disabledTextInput" placeholder=" input" />
                     </Form.Group>
                     <Form.Group className="mb-3">
-                    <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+                    <Form.Label htmlFor="disabledSelect"> select menu</Form.Label>
                     <Form.Select id="disabledSelect">
                         <option>Disabled select</option>
                     </Form.Select>
@@ -23,6 +33,10 @@ class Order extends React.Component {
                         id="disabledFieldsetCheck"
                         label="Can't check this"
                     />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <AddressSuggestions token="cac1688a9d096f9d41f19559d9d69658e136f616" value={this.state.address}
+                            onChange={(e) => {this.setState({address: e})}} />
                     </Form.Group>
                     <Button type="submit">Submit</Button>
                 </fieldset>
